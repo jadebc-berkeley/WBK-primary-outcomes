476a5e8d2122f0490da617fb77622a7eea65e814
“amertens” <amertens@berkeley.edu>
2017-10-22 23:22:13 -0700

    Updated axis labeling

--- primary/figures/diar-calendar-time-figure.R
+++ primary/figures/diar-calendar-time-figure.R
@@ -2,7 +2,7 @@
 # diar-calendar-time.R
 # andrew mertens (amertens@berkeley.edu)
 #
-# summarize diarrhea prevalence
+# summarize Diarrhoea prevalence
 # by calendar time
 #---------------------------------
 
@@ -147,7 +147,7 @@ cols <- c(corange,cblue)
 plot(diar_c$month[diar_c$month<="2015-07-01"],
      diar_c$mean[diar_c$month<="2015-07-01"],
      type="n",
-     ylim=range(ytics),ylab="7-day Diarrhea prevalence (%)",yaxt="n",
+     ylim=range(ytics),ylab="7-day Diarrhoea prevalence (%)",yaxt="n",
      xlim=range(xtics),xlab="",xaxt="n",
      bty="n",las=1)
 axis(1,at=xtics,las=2,labels=format(xtics,"%m"),cex.axis=0.6)
@@ -195,7 +195,7 @@ cols <- c(corange,cblue)
 plot(diar_c$month[diar_c$month<="2015-07-01"],
      diar_c$mean[diar_c$month<="2015-07-01"],
      type="n",
-     ylim=range(ytics),ylab="7- day Diarrhea prevalence (%)",yaxt="n",
+     ylim=range(ytics),ylab="7- day Diarrhoea prevalence (%)",yaxt="n",
      xlim=range(xtics),xlab="",xaxt="n",
      bty="n",las=1)
 axis(1,at=xtics,las=2,labels=format(xtics,"%m"),cex.axis=0.6)
@@ -240,7 +240,7 @@ cols <- c(corange,cblue)
 plot(diar_c$month[diar_c$month<="2015-07-01"],
      diar_c$mean[diar_c$month<="2015-07-01"],
      type="n",
-     ylim=range(ytics),ylab="7-day Diarrhea prevalence (%)",yaxt="n",
+     ylim=range(ytics),ylab="7-day Diarrhoea prevalence (%)",yaxt="n",
      xlim=range(xtics),xlab="",xaxt="n",
      bty="n",las=1)
   axis(1,at=xtics,las=2,labels=format(xtics,"%m"),cex.axis=0.6)
@@ -494,7 +494,7 @@ xtics3 <- as.Date(c("2015-03-01","2016-02-01"))
 plot(diar_c$month[diar_c$month<="2015-07-01"],
      diar_c$mean[diar_c$month<="2015-07-01"],
      type="n",
-     ylim=range(ytics),ylab="7-day Diarrhea prevalence (%)",yaxt="n",
+     ylim=range(ytics),ylab="7-day Diarrhoea prevalence (%)",yaxt="n",
      xlim=range(xtics),xlab="",xaxt="n",
      bty="n",las=1)
   axis(1,at=xtics,las=2,labels=format(xtics,"%m"),cex.axis=0.6)
