a36af8b8f13ff6579d50de1e277e4c94504e8ac5
Ruwan23 <51462111+Ruwan23@users.noreply.github.com>
2020-02-12 10:14:00 -0800

    - replaced library calls with sourced config file - eliminated unncessary directory definitions - fixed path for sourcing base programs - updated dataset read-in file paths to cohere with local directory set by here package - enclosed file paths with here() to cohere with local directory set by here package - fixed dated parameter name in approximate() function (from coin package), from B-->nresample

--- primary/analysis/1-diar/1d-diarr-perm-test.R
+++ primary/analysis/1-diar/1d-diarr-perm-test.R
@@ -17,17 +17,11 @@
 # by Jade Benjamin-Chung (jadebc@berkeley.edu)
 ##############################################
 
-rm(list=ls())
-library(reshape2)
-library(coin)
 
-# define directories
-source.dir="~/documents/crg/wash-benefits/kenya/src/primary/analysis/"
-data.dir="~/Dropbox/WASHB-Kenya-Data/1-primary-outcome-datasets/Public/"
-res.dir="~/Dropbox/WBK-primary-analysis/results/jade/"
+source(here::here("primary/analysis/0-config.R"))
+source(here("primary/analysis/0-base-programs.R"))
 
-data=read.csv(paste0(data.dir,"washb-kenya-diar-public.csv"),stringsAsFactors=TRUE)
-source(paste0(source.dir,"0-base-programs.R"))
+data=read.csv(here("primary/data/washb-kenya-diar-public.csv"),stringsAsFactors=TRUE)
 
 d=preprocess.diarr(data)
 
@@ -38,43 +32,43 @@ d=preprocess.diarr(data)
 P.df=coin.prep(d,tx="Passive Control",cont="Control",y="diarr7")
 set.seed(67890)
 P.permtest=wilcoxsign_test(diarr7~tr | block, data=P.df,
-       distribution=approximate(B=100000))
+       distribution=approximate(nresample=100000))
 P.perm.p=pvalue(P.permtest)
 
 W.df=coin.prep(d,tx="Water",cont="Control",y="diarr7")
 set.seed(67890)
 W.permtest=wilcoxsign_test(diarr7~tr | block, data=W.df,
-   distribution=approximate(B=100000))
+   distribution=approximate(nresample=100000))
 W.perm.p=pvalue(W.permtest)
 
 WSH.S.df=coin.prep(d,tx="Sanitation",cont="Control",y="diarr7")
 set.seed(67890)
 WSH.S.permtest=wilcoxsign_test(diarr7~tr | block, data=WSH.S.df,
-   distribution=approximate(B=100000))
+   distribution=approximate(nresample=100000))
 WSH.S.perm.p=pvalue(WSH.S.permtest)
 
 H.df=coin.prep(d,tx="Handwashing",cont="Control",y="diarr7")
 set.seed(67890)
 H.permtest=wilcoxsign_test(diarr7~tr | block, data=H.df,
-   distribution=approximate(B=100000))
+   distribution=approximate(nresample=100000))
 H.perm.p=pvalue(H.permtest)
 
 WSH.df=coin.prep(d,tx="WSH",cont="Control",y="diarr7")
 set.seed(67890)
 WSH.permtest=wilcoxsign_test(diarr7~tr | block, data=WSH.df,
-   distribution=approximate(B=100000))
+   distribution=approximate(nresample=100000))
 WSH.perm.p=pvalue(WSH.permtest)
 
 N.df=coin.prep(d,tx="Nutrition",cont="Control",y="diarr7")
 set.seed(67890)
 N.permtest=wilcoxsign_test(diarr7~tr | block, data=N.df,
-   distribution=approximate(B=100000))
+   distribution=approximate(nresample=100000))
 N.perm.p=pvalue(N.permtest)
 
 WSHN.df=coin.prep(d,tx="Nutrition + WSH",cont="Control",y="diarr7")
 set.seed(67890)
 WSHN.permtest=wilcoxsign_test(diarr7~tr | block, data=WSHN.df,
-   distribution=approximate(B=100000))
+   distribution=approximate(nresample=100000))
 WSHN.perm.p=pvalue(WSHN.permtest)
 
 diar_h1_pval_unadj_j=data.frame(perm.pvalue=c(P.perm.p,W.perm.p,WSH.S.perm.p,H.perm.p,
@@ -90,19 +84,19 @@ rownames(diar_h1_pval_unadj_j)=c("Passive Control vs. C","Water vs. C",
 WSH.W.df=coin.prep(d,tx="WSH",cont="Water",y="diarr7")
 set.seed(67890)
 WSH.W.permtest=wilcoxsign_test(diarr7~tr | block, data=WSH.W.df,
-   distribution=approximate(B=100000))
+   distribution=approximate(nresample=100000))
 WSH.W.perm.p=pvalue(WSH.W.permtest)
 
 WSH.S.df=coin.prep(d,tx="WSH",cont="Sanitation",y="diarr7")
 set.seed(67890)
 WSH.S.permtest=wilcoxsign_test(diarr7~tr | block, data=WSH.S.df,
-   distribution=approximate(B=100000))
+   distribution=approximate(nresample=100000))
 WSH.S.perm.p=pvalue(WSH.S.permtest)
 
 WSH.H.df=coin.prep(d,tx="WSH",cont="Handwashing",y="diarr7")
 set.seed(67890)
 WSH.H.permtest=wilcoxsign_test(diarr7~tr | block, data=WSH.H.df,
-    distribution=approximate(B=100000))
+    distribution=approximate(nresample=100000))
 WSH.H.perm.p=pvalue(WSH.H.permtest)
 
 diar_h2_pval_unadj_j=data.frame(perm.pvalue=c(WSH.W.perm.p,WSH.S.perm.p,WSH.H.perm.p))
@@ -110,7 +104,7 @@ rownames(diar_h2_pval_unadj_j)=c("WSH vs. Water",
                                  "WSH vs. Sanitation", "WSH vs. Handwashing")
 
 save(diar_h1_pval_unadj_j,diar_h2_pval_unadj_j,
-     file=paste0(res.dir,"diarr_pval_unadj.RData"))
+     file=here("primary/res_data/diarr_pval_unadj.RData"))
 
 
 diar_h1_pval_unadj_j
