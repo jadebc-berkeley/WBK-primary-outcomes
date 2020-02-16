5d06b8aed694152ab29b6f04a96349e15e1dbe96
Ruwan23 <51462111+Ruwan23@users.noreply.github.com>
2020-02-15 21:39:30 -0800

    - replaced library calls with sourcing config file - enclosed paths with here() to integrate with working directory set up by here package - updated file paths to reflect public-facing directories - eliminated unnecessary directory definitions, as paths are handled with here package - pvalue() function appears in both coin and scales; coin version is wanted here, thus called coin-specific pvalue() function

--- primary/analysis/8-wasting/8d-wasting-perm-test.R
+++ primary/analysis/8-wasting/8d-wasting-perm-test.R
@@ -15,20 +15,11 @@
 
 # by Jade Benjamin-Chung (jadebc@berkeley.edu)
 ##############################################
+source(here::here("primary/analysis/0-config.R"))
+source(here("primary/analysis/0-base-programs.R"))
 
-rm(list=ls())
-library(reshape2)
-library(coin)
-
-# define directories
-source.dir="~/documents/crg/wash-benefits/kenya/src/primary/analysis/"
-data.dir="~/Dropbox/WASHB-Kenya-Data/1-primary-outcome-datasets/Public/"
-res.dir="~/Dropbox/WBK-primary-analysis/results/jade/"
-
-m=read.csv(paste0(data.dir,"washb-kenya-midline-anthro-public.csv"))
-e=read.csv(paste0(data.dir,"washb-kenya-endline-anthro-public.csv"))
-
-source(paste0(source.dir,"0-base-programs.R"))
+m=read.csv(here("primary/data/washb-kenya-midline-anthro-public.csv"))
+e=read.csv(here("primary/data/washb-kenya-endline-anthro-public.csv"))
 
 m=preprocess.anthro(m, "wasted")
 e=preprocess.anthro(e, "wasted")
@@ -41,44 +32,44 @@ e=preprocess.anthro(e, "wasted")
 P.t1.df=coin.prep(m,tx="Passive Control",cont="Control",y="wasted")
 set.seed(67890)
 P.t1.permtest=wilcoxsign_test(wasted~tr | block, data=P.t1.df,
-       distribution=approximate(B=100000))
-P.t1.perm.p=pvalue(P.t1.permtest)
+       distribution=approximate(nresample=100000))
+P.t1.perm.p=coin::pvalue(P.t1.permtest)
 
 W.t1.df=coin.prep(m,tx="Water",cont="Control",y="wasted")
 set.seed(67890)
 W.t1.permtest=wilcoxsign_test(wasted~tr | block, data=W.t1.df,
-   distribution=approximate(B=100000))
-W.t1.perm.p=pvalue(W.t1.permtest)
+   distribution=approximate(nresample=100000))
+W.t1.perm.p=coin::pvalue(W.t1.permtest)
 
 WSH.S.t1.df=coin.prep(m,tx="Sanitation",cont="Control",y="wasted")
 set.seed(67890)
 WSH.S.t1.permtest=wilcoxsign_test(wasted~tr | block, data=WSH.S.t1.df,
-   distribution=approximate(B=100000))
-WSH.S.t1.perm.p=pvalue(WSH.S.t1.permtest)
+   distribution=approximate(nresample=100000))
+WSH.S.t1.perm.p=coin::pvalue(WSH.S.t1.permtest)
 
 H.t1.df=coin.prep(m,tx="Handwashing",cont="Control",y="wasted")
 set.seed(67890)
 H.t1.permtest=wilcoxsign_test(wasted~tr | block, data=H.t1.df,
-   distribution=approximate(B=100000))
-H.t1.perm.p=pvalue(H.t1.permtest)
+   distribution=approximate(nresample=100000))
+H.t1.perm.p=coin::pvalue(H.t1.permtest)
 
 WSH.t1.df=coin.prep(m,tx="WSH",cont="Control",y="wasted")
 set.seed(67890)
 WSH.t1.permtest=wilcoxsign_test(wasted~tr | block, data=WSH.t1.df,
-   distribution=approximate(B=100000))
-WSH.t1.perm.p=pvalue(WSH.t1.permtest)
+   distribution=approximate(nresample=100000))
+WSH.t1.perm.p=coin::pvalue(WSH.t1.permtest)
 
 N.t1.df=coin.prep(m,tx="Nutrition",cont="Control",y="wasted")
 set.seed(67890)
 N.t1.permtest=wilcoxsign_test(wasted~tr | block, data=N.t1.df,
-   distribution=approximate(B=100000))
-N.t1.perm.p=pvalue(N.t1.permtest)
+   distribution=approximate(nresample=100000))
+N.t1.perm.p=coin::pvalue(N.t1.permtest)
 
 WSHN.t1.df=coin.prep(m,tx="Nutrition + WSH",cont="Control",y="wasted")
 set.seed(67890)
 WSHN.t1.permtest=wilcoxsign_test(wasted~tr | block, data=WSHN.t1.df,
-   distribution=approximate(B=100000))
-WSHN.t1.perm.p=pvalue(WSHN.t1.permtest)
+   distribution=approximate(nresample=100000))
+WSHN.t1.perm.p=coin::pvalue(WSHN.t1.permtest)
 
 wast_t1_h1_pval_unadj_j=data.frame(perm.pvalue=c(P.t1.perm.p,W.t1.perm.p,WSH.S.t1.perm.p,H.t1.perm.p,
                          WSH.t1.perm.p,N.t1.perm.p,WSHN.t1.perm.p))
@@ -94,44 +85,44 @@ rownames(wast_t1_h1_pval_unadj_j)=c("Passive Control vs. C","Water vs. C",
 P.t2.df=coin.prep(e,tx="Passive Control",cont="Control",y="wasted")
 set.seed(67890)
 P.t2.permtest=wilcoxsign_test(wasted~tr | block, data=P.t2.df,
-       distribution=approximate(B=100000))
-P.t2.perm.p=pvalue(P.t2.permtest)
+       distribution=approximate(nresample=100000))
+P.t2.perm.p=coin::pvalue(P.t2.permtest)
 
 W.t2.df=coin.prep(e,tx="Water",cont="Control",y="wasted")
 set.seed(67890)
 W.t2.permtest=wilcoxsign_test(wasted~tr | block, data=W.t2.df,
-   distribution=approximate(B=100000))
-W.t2.perm.p=pvalue(W.t2.permtest)
+   distribution=approximate(nresample=100000))
+W.t2.perm.p=coin::pvalue(W.t2.permtest)
 
 WSH.S.t2.df=coin.prep(e,tx="Sanitation",cont="Control",y="wasted")
 set.seed(67890)
 WSH.S.t2.permtest=wilcoxsign_test(wasted~tr | block, data=WSH.S.t2.df,
-   distribution=approximate(B=100000))
-WSH.S.t2.perm.p=pvalue(WSH.S.t2.permtest)
+   distribution=approximate(nresample=100000))
+WSH.S.t2.perm.p=coin::pvalue(WSH.S.t2.permtest)
 
 H.t2.df=coin.prep(e,tx="Handwashing",cont="Control",y="wasted")
 set.seed(67890)
 H.t2.permtest=wilcoxsign_test(wasted~tr | block, data=H.t2.df,
-   distribution=approximate(B=100000))
-H.t2.perm.p=pvalue(H.t2.permtest)
+   distribution=approximate(nresample=100000))
+H.t2.perm.p=coin::pvalue(H.t2.permtest)
 
 WSH.t2.df=coin.prep(e,tx="WSH",cont="Control",y="wasted")
 set.seed(67890)
 WSH.t2.permtest=wilcoxsign_test(wasted~tr | block, data=WSH.t2.df,
-   distribution=approximate(B=100000))
-WSH.t2.perm.p=pvalue(WSH.t2.permtest)
+   distribution=approximate(nresample=100000))
+WSH.t2.perm.p=coin::pvalue(WSH.t2.permtest)
 
 N.t2.df=coin.prep(e,tx="Nutrition",cont="Control",y="wasted")
 set.seed(67890)
 N.t2.permtest=wilcoxsign_test(wasted~tr | block, data=N.t2.df,
-   distribution=approximate(B=100000))
-N.t2.perm.p=pvalue(N.t2.permtest)
+   distribution=approximate(nresample=100000))
+N.t2.perm.p=coin::pvalue(N.t2.permtest)
 
 WSHN.t2.df=coin.prep(e,tx="Nutrition + WSH",cont="Control",y="wasted")
 set.seed(67890)
 WSHN.t2.permtest=wilcoxsign_test(wasted~tr | block, data=WSHN.t2.df,
-   distribution=approximate(B=100000))
-WSHN.t2.perm.p=pvalue(WSHN.t2.permtest)
+   distribution=approximate(nresample=100000))
+WSHN.t2.perm.p=coin::pvalue(WSHN.t2.permtest)
 
 wast_t2_h1_pval_unadj_j=data.frame(perm.pvalue=c(P.t2.perm.p,W.t2.perm.p,WSH.S.t2.perm.p,H.t2.perm.p,
                          WSH.t2.perm.p,N.t2.perm.p,WSHN.t2.perm.p))
@@ -148,14 +139,14 @@ rownames(wast_t2_h1_pval_unadj_j)=c("Passive Control vs. C","Water vs. C",
 WSHN.N.t1.df=coin.prep(m,tx="Nutrition + WSH",cont="Nutrition",y="wasted")
 set.seed(67890)
 WSHN.N.t1.permtest=wilcoxsign_test(wasted~tr | block, data=WSHN.N.t1.df,
-   distribution=approximate(B=100000))
-WSHN.N.t1.perm.p=pvalue(WSHN.N.t1.permtest)
+   distribution=approximate(nresample=100000))
+WSHN.N.t1.perm.p=coin::pvalue(WSHN.N.t1.permtest)
 
 WSHN.WSH.t1.df=coin.prep(m,tx="Nutrition + WSH",cont="WSH",y="wasted")
 set.seed(67890)
 WSHN.WSH.t1.permtest=wilcoxsign_test(wasted~tr | block, data=WSHN.WSH.t1.df,
-   distribution=approximate(B=100000))
-WSHN.WSH.t1.perm.p=pvalue(WSHN.WSH.t1.permtest)
+   distribution=approximate(nresample=100000))
+WSHN.WSH.t1.perm.p=coin::pvalue(WSHN.WSH.t1.permtest)
 
 wast_t1_h3_pval_unadj_j=data.frame(perm.pvalue=c(WSHN.N.t1.perm.p,WSHN.WSH.t1.perm.p))
 rownames(wast_t1_h3_pval_unadj_j)=c("Nutrition + WSH vs. Nutrition",
@@ -169,14 +160,14 @@ rownames(wast_t1_h3_pval_unadj_j)=c("Nutrition + WSH vs. Nutrition",
 WSHN.N.t2.df=coin.prep(e,tx="Nutrition + WSH",cont="Nutrition",y="wasted")
 set.seed(67890)
 WSHN.N.t2.permtest=wilcoxsign_test(wasted~tr | block, data=WSHN.N.t2.df,
-   distribution=approximate(B=100000))
-WSHN.N.t2.perm.p=pvalue(WSHN.N.t2.permtest)
+   distribution=approximate(nresample=100000))
+WSHN.N.t2.perm.p=coin::pvalue(WSHN.N.t2.permtest)
 
 WSHN.WSH.t2.df=coin.prep(e,tx="Nutrition + WSH",cont="WSH",y="wasted")
 set.seed(67890)
 WSHN.WSH.t2.permtest=wilcoxsign_test(wasted~tr | block, data=WSHN.WSH.t2.df,
-   distribution=approximate(B=100000))
-WSHN.WSH.t2.perm.p=pvalue(WSHN.WSH.t2.permtest)
+   distribution=approximate(nresample=100000))
+WSHN.WSH.t2.perm.p=coin::pvalue(WSHN.WSH.t2.permtest)
 
 wast_t2_h3_pval_unadj_j=data.frame(perm.pvalue=c(WSHN.N.t2.perm.p,WSHN.WSH.t2.perm.p))
 rownames(wast_t2_h3_pval_unadj_j)=c("Nutrition + WSH vs. Nutrition",
@@ -187,10 +178,9 @@ rownames(wast_t2_h3_pval_unadj_j)=c("Nutrition + WSH vs. Nutrition",
 #----------------------------------------------
 save(wast_t1_h1_pval_unadj_j,
      wast_t2_h1_pval_unadj_j,
-
      wast_t1_h3_pval_unadj_j,
      wast_t2_h3_pval_unadj_j,
-     file=paste0(res.dir,"wast_pval_unadj.RData"))
+     file=here("primary/res_data/wast_pval_unadj.RData"))
 
 
 wast_t1_h1_pval_unadj_j
