b3b20768e94233c4ceb1438fed7eed20588932a0
Ruwan23 <51462111+Ruwan23@users.noreply.github.com>
2020-02-22 12:49:06 -0800

    packages are now installed and loaded if not already installed, and loaded if already installed; this removes the need for users to individually install needed packages

--- primary/analysis/0-config.R
+++ primary/analysis/0-config.R
@@ -1,26 +1,161 @@
 rm(list=ls())
-library(plyr)
-library(dplyr)
-library(coin)
-library(tidyverse)
-library(here)
-library(metafor)
-library(coin)
-library(metafor)
-library(tmle)
-library(SuperLearner)
-library(MASS)
-library(lmtest)
-library(sandwich)
-library(xtable)
-library(scales)
-library(RColorBrewer)
-library(washb)
-library(devtools)
-library(reshape2)
-library(rgeos)
-library(maptools)
-library(rgdal)
+if(!require(plyr)){
+  install.packages("plyr")
+  library(plyr)
+} else{
+  library(plyr)
+}
+
+if(!require(dplyr)){
+  install.packages("dplyr")
+  library(dplyr)
+} else{
+  library(dplyr)
+}
+
+if(!require(coin)){
+  install.packages("coin")
+  library(coin)
+} else{
+  library(coin)
+}
+
+if(!require(tidyverse)){
+  install.packages("tidyverse")
+  library(tidyverse)
+} else{
+  library(tidyverse)
+}
+
+if(!require(metafor)){
+  install.packages("metafor")
+  library(metafor)
+} else{
+  library(metafor)
+}
+
+if(!require(tmle)){
+  install.packages("tmle")
+  library(tmle)
+} else{
+  library(tmle)
+}
+
+if(!require(SuperLearner)){
+  install.packages("SuperLearner")
+  library(SuperLearner)
+} else{
+  library(SuperLearner)
+}
+
+if(!require(MASS)){
+  install.packages("MASS")
+  library(MASS)
+} else{
+  library(MASS)
+}
+
+if(!require(lmtest)){
+  install.packages("lmtest")
+  library(lmtest)
+} else{
+  library(lmtest)
+}
+
+if(!require(sandwich)){
+  install.packages("sandwich")
+  library(sandwich)
+} else{
+  library(sandwich)
+}
+
+if(!require(xtable)){
+  install.packages("xtable")
+  library(xtable)
+} else{
+  library(xtable)
+}
+
+if(!require(scales)){
+  install.packages("scales")
+  library(scales)
+} else{
+  library(scales)
+}
+
+if(!require(RColorBrewer)){
+  install.packages("RColorBrewer")
+  library(RColorBrewer)
+} else{
+  library(RColorBrewer)
+}
+
+if(!require(devtools)){
+  install.packages("devtools")
+  library(devtools)
+} else{
+  library(devtools)
+}
+
+if(!require(reshape2)){
+  install.packages("reshape2")
+  library(reshape2)
+} else{
+  library(reshape2)
+}
+
 if(!require(washb)){
   install_github("ben-arnold/washb")
+  library(washb)
+} else{
+  library(washb)
+}
+
+if(!require(rgeos)){
+  install.packages("rgeos")
+  library(rgeos)
+} else{
+  library(rgeos)
+}
+
+if(!require(maptools)){
+  install.packages("maptools")
+  library(maptools)
+} else{
+  library(maptools)
+}
+
+if(!require(rgdal)){
+  install.packages("rgdal")
+  library(rgdal)
+} else{
+  library(rgdal)
+}
+
+if(!require(magrittr)){
+  install.packages("magrittr")
+  library(magrittr)
+} else{
+  library(magrittr)
+}
+
+if(!require(foreign)){
+  install.packages("foreign")
+  library(foreign)
+} else{
+  library(foreign)
+}
+
+if(!require(rmarkdown)){
+  install.packages("rmarkdown")
+  library(rmarkdown)
+} else{
+  library(rmarkdown)
+}
+
+if(!require(ggthemes)){
+  install.packages("ggthemes")
+  library(ggthemes)
+} else{
+  library(ggthemes)
 }
